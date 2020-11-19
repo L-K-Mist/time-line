7749ca1bfbffa5e3e217514563682c0c06e63d21
Dylan van den Bosch <dylan@vandenbosch.co.za>
2020-11-19 16:49:23 +0200

    Year moves (but wrong position)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <TimeLine />
+  <TimeLine @click="moveYearTopLeft" />
 </template>
 
 <script>
@@ -16,21 +16,45 @@ export default defineComponent({
     function viewBoxString(frame) {
       return `${frame.x} ${frame.y} ${frame.width} ${frame.height}`;
     }
-
+    let svg;
     function zoomToView(elementId) {
       const element = document.getElementById(elementId);
       const box = element.getBBox();
-      return gsap.to("#svg-timeline", {
+      return gsap.to(svg, {
         duration: 1,
         attr: { viewBox: viewBoxString(box) },
       });
     }
+
+    function screenToSVGPoint(screenPoint, svgElement) {
+      const point = svgElement.createSVGPoint();
+      point.x = screenPoint.x;
+      point.y = screenPoint.y;
+      const svgPoint = point.matrixTransform(
+        svgElement.getScreenCTM().inverse()
+      );
+      return svgPoint;
+    }
+
+    function moveYearTopLeft() {
+      const screenPosition = { x: 30, y: 30 };
+      const svgPosition = screenToSVGPoint(screenPosition, svg);
+      gsap.to("#year2020", {
+        x: svgPosition.x,
+        y: svgPosition.y,
+      });
+    }
+
     onMounted(() => {
+      svg = document.getElementById("svg-timeline");
       setTimeout(() => {
         zoomToView("rect968");
       }, 500);
       // test zoom
     });
+    return {
+      moveYearTopLeft,
+    };
   },
 });
 </script>
